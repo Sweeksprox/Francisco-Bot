34ec71914d655de0d63d21453de39535af5bfaec
Sweeks <john.suihkonen@gmail.com>
2016-04-07 16:47:48 -0500

    Added files via upload

---
+++ config_example.js
@@ -0,0 +1,13 @@
+// config.js
+
+var config = {};
+
+config.botName = "bot name";
+config.auth = "oauth:xxxxxxxxxxxx"; //you can get this from here: http://twitchapps.com/tmi/
+config.channel = "channelname"
+
+// You can find more time zones here under the "TZ" column: 
+// www.wikipedia.org/wiki/List_of_tz_database_time_zones
+config.timeZone = "America/Los_Angeles";
+
+module.exports = config;
